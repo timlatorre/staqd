9cab10d89d8a47abf7528bc2ea92b3d502b5e7df
Tim LaTorre <timlatorre@gmail.com>
2012-12-03 11:17:57 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Staqd::Application.config.secret_token = '35f1a510c946a45b06fba7ad0d6a307020652b4b1e3da5792584c35fb70569492a11dbe8347cfe85aed81d574a17b93b089a961eb17d3d1942cbc942eef7ea9d'
